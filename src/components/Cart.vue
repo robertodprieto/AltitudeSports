27c108ba5ea32b9834de88b1994016c558a2ced3
robertodprieto <robertodprieto@gmail.com>
2022-01-16 15:12:13 -0300

    feat: calculate subtotal and total price

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -7,67 +7,91 @@
     <section>
       <table>
         <thead>
-        <tr>
-          <th>Item</th>
-          <th>Quantity</th>
-          <th class="numeric">Price</th>
-        </tr>
+          <tr>
+            <th>Item</th>
+            <th>Quantity</th>
+            <th class="numeric">Price</th>
+          </tr>
         </thead>
         <tbody>
-        <tr v-for="(cartItem, index) in cart.items" :key="index" :data-testid="'cart-item-' + index">
-          <td data-testid="cart-item-name">{{ cartItem.item }}</td>
-          <td data-testid="cart-item-quantity">{{ cartItem.quantity }}</td>
-          <td class="numeric" data-testid="cart-item-price">${{ cartItem.price }}</td>
-        </tr>
+          <tr
+            v-for="(cartItem, index) in cart.items"
+            :key="index"
+            :data-testid="'cart-item-' + index"
+          >
+            <td data-testid="cart-item-name">{{ cartItem.item }}</td>
+            <td data-testid="cart-item-quantity">{{ cartItem.quantity }}</td>
+            <td class="numeric" data-testid="cart-item-price">
+              ${{ cartItem.price }}
+            </td>
+          </tr>
         </tbody>
       </table>
 
-      <div class="layout-row justify-content-between align-items-center px-8 mx-12">
+      <div
+        class="layout-row justify-content-between align-items-center px-8 mx-12"
+      >
         <h5>Select Coupon</h5>
-        <select data-testid="cart-coupon"
-                class="coupon-select">
+        <select
+          data-testid="cart-coupon"
+          class="coupon-select"
+          v-model="selectedDiscount"
+        >
           <option :value="0">None</option>
           <option :value="10">OFF10</option>
           <option :value="20">OFF20</option>
         </select>
       </div>
       <ul class="bordered inset ma-0 px-8 mt-30">
-        <li class="layout-row justify-content-between py-12 caption font-weight-light">
+        <li
+          class="layout-row justify-content-between py-12 caption font-weight-light"
+        >
           <span>Subtotal</span>
-          <span data-testid="cart-subtotal">${{ cart.subTotal }}</span>
+          <span data-testid="cart-subtotal">${{ subTotal }}</span>
         </li>
-        <li class="layout-row justify-content-between py-12 caption font-weight-light">
+        <li
+          class="layout-row justify-content-between py-12 caption font-weight-light"
+        >
           <span>Discount (-)</span>
-          <span class="discount" data-testid="cart-discount">${{ cart.discount }}</span>
+          <span class="discount" data-testid="cart-discount"
+            >${{ discount }}</span
+          >
         </li>
         <li class="layout-row justify-content-between py-12 font-weight-bold">
           <span>Total</span>
-          <span data-testid="cart-total">${{ cart.totalPrice }}</span>
+          <span data-testid="cart-total">${{ totalPrice }}</span>
         </li>
       </ul>
-
     </section>
   </div>
-
 </template>
 
 <script>
 export default {
   name: "Cart",
   props: {
-    cart: Object
+    cart: Object,
   },
   data() {
     return {
-      coupon : 0
-    }
+      coupon: 0,
+      selectedDiscount: 0,
+    };
   },
-  methods : {
-
-  }
-}
+  computed: {
+    subTotal() {
+      let subtotal = 0;
+      this.cart.items.forEach((item) => (subtotal += item.price));
+      return subtotal;
+    },
+    discount() {
+      return (this.subTotal * this.selectedDiscount) / 100;
+    },
+    totalPrice() {
+      return this.subTotal - this.discount;
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
