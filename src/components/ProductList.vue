f32e0f4a7ddc4e351fab849a77211bb70f3fe46f
robertodprieto <robertodprieto@gmail.com>
2022-01-16 15:13:05 -0300

    feat: add and remove products from the cart

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,48 +1,69 @@
 <template>
   <div class="layout-row wrap justify-content-center">
-    <section v-for="(product, index) in products"
-             :key="index"
-             :data-testid="'product-item-' + index"
-             class="w-30 product-item">
+    <section
+      v-for="(product, index) in products"
+      :key="index"
+      :data-testid="'product-item-' + index"
+      class="w-30 product-item"
+    >
       <div class="card ma-16">
-        <img :src="product.image" class="product-image" :alt="product.name"/>
+        <img :src="product.image" class="product-image" :alt="product.name" />
         <div class="card-text pa-4">
           <h5 class="ma-0 text-center">{{ product.name }}</h5>
           <p class="ma-0 mt-8 text-center">${{ product.price }}</p>
         </div>
         <div class="card-actions justify-content-center pa-4">
-          <button class="x-small outlined" data-testid="btn-item-add">
+          <button
+            class="x-small outlined"
+            data-testid="btn-item-add"
+            :disabled="checkProductInCart(product)"
+            @click="addToCart(product)"
+          >
             Add To Cart
           </button>
 
-          <button class="x-small danger" data-testid="btn-item-remove">
+          <button
+            class="x-small danger"
+            data-testid="btn-item-remove"
+            :disabled="!checkProductInCart(product)"
+            @click="removeFromCart(product)"
+          >
             Remove
           </button>
         </div>
       </div>
     </section>
   </div>
-
 </template>
 
 <script>
 export default {
   name: "ProductList",
   props: {
-    products: Array
+    products: Array,
+    cart: Object,
   },
-  methods : {
-
-  }
-}
+  methods: {
+    addToCart(product) {
+      this.$emit("add-to-cart", product);
+    },
+    removeFromCart(product) {
+      this.$emit("remove-from-cart", product);
+    },
+    checkProductInCart(product) {
+      return this.cart.items.filter((item) => item.id === product.id).length ===
+        0
+        ? false
+        : true;
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss">
-
 .product-image {
   margin: 32px 30%;
   width: 40%;
   min-height: 90px;
 }
-
 </style>
